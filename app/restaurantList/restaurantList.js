7db84e45c3a24e785b82931b223d8758e244f294
Lukasz Chylinski <l.chylinski@wp.pl>
2015-08-13 16:05:10 +0200

    update config

--- app/restaurantList/restaurantList.js
+++ app/restaurantList/restaurantList.js
@@ -13,9 +13,53 @@ angular.module('myApp.restaurantList', ['ngRoute','ui.bootstrap'])
   $scope.variable = "Our tekst";
   $scope.progress = 65;
 
+<<<<<<< Updated upstream
   $scope.showText = function() {
     alert($scope.variable);
   }
+=======
+}]).controller('RestaurantListController', function($rootScope, $scope, $location, $routeParams) {
+      $scope.variable = "Our tekst";
+      $scope.progress = 65;
+
+      $scope.showText = function() {
+        alert($scope.variable);
+      }
+
+      $scope.oneAtATime = true;
+
+      $scope.groups = [
+        {
+          title: 'Dynamic Group Header - 1',
+          content: 'Dynamic Group Body - 1'
+        },
+        {
+          title: 'Dynamic Group Header - 2',
+          content: 'Dynamic Group Body - 2'
+        }
+      ];
+
+      $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+      $scope.addItem = function() {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+      };
+
+      $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+      };
+
+
+      $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+      };
+
+
+    });
+>>>>>>> Stashed changes
 
   $scope.oneAtATime = true;
 
