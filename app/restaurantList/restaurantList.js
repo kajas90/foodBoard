e355188494e2c2f21f9bd91f39e3b3e2459e6fa2
wnukwiktor <panwiktor@gmail.com>
2015-08-17 11:59:55 +0200

    json data
    a

--- app/restaurantList/restaurantList.js
+++ app/restaurantList/restaurantList.js
@@ -9,39 +9,24 @@ angular.module('myApp.restaurantList', ['ngRoute','ui.bootstrap'])
   });
 }])
 
-.controller('restaurantListCtrl', function($rootScope, $scope, $location, $routeParams) {
-  $scope.variable = "Our tekst";
-  $scope.progress = 65;
+.controller('restaurantListCtrl', ['$scope', '$http', '$rootScope', '$location', '$routeParams', function($scope, $http, $rootScope, $location, $routeParams) {
 
-  $scope.showText = function() {
-    alert($scope.variable);
-  }
+      $http.get('js/data.json').success(function(data) {
 
+          $scope.groups = data;
+
+          console.log('poprawnie pobrano dane z js/data.json');
+      });
+      $http.get('js/data.json').error(function(data) {
+
+        console.log('nie zaladowano js/data.json');
+      });
+      
+
+  //accordion
   $scope.oneAtATime = true;
 
-  $scope.groups = [
-    {
-      title: 'jakisTytul',
-      content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
-      ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ' +
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
-      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-      logo : 'LOGO',
-      name : 'u Skrzypka',
-      type : 'Pizzeria'
-    },
-    {
-      title: 'tytul2',
-      content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
-      ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-      logo : 'LOGO',
-      name : 'SurfBurger',
-      type : 'Burger'
-    }
-  ];
-
   $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
   $scope.addItem = function() {
     var newItemNo = $scope.items.length + 1;
     $scope.items.push('Item ' + newItemNo);
@@ -56,4 +41,4 @@ angular.module('myApp.restaurantList', ['ngRoute','ui.bootstrap'])
     $location.path('/restaurantAdd');
   };
 
-});
+}]);
